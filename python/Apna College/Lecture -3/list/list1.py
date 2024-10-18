f6442a0464b5ef86890d05af85ea626ee203645d
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-10-18 14:23:13 +0600

    Add files via upload

---
+++ python/Apna College/Lecture -3/list/list1.py
@@ -0,0 +1,5 @@
+marks = [87,94,76,63,48]
+print(marks[1:])
+
+marks = [87,94,76,63,48]
+print(marks[-3:-1])
---
+++ python/Apna College/Lecture -3/list/sort.py
@@ -0,0 +1,4 @@
+list = [2,1,3]
+print(list.sort())
+print(list.sort(reverse=True))
+print(list)
