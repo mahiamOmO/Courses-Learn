dbdd7ba1bebc6405fdc8d69584470e734f226303
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-10-28 22:19:41 +0600

    Delete python/python practice directory

--- python/python practice/Datatype.py
+++
@@ -1,9 +0,0 @@
-name = 'Smith'
-age = 30
-height = 1.75
-is_adult = True
-
-print('Name:',name)
-print('Age:',age)
-print('Height:',height)
-print('Is Adult:',is_adult)
--- python/python practice/datatype_conversion1.py
+++
@@ -1,8 +0,0 @@
-grades = [90, 85, 92, 88]
-coordinates = (3, 7)
-person = {'name': 'Alice', 'age': 29, 'city': 'UK'}
-
-print('grades:', type(grades))
-print('coordinates:', type(coordinates))
-print('person:', type(person))
-
