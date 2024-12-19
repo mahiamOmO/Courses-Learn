ab89063a7d7a20d1b2d5edaf68733ab2592910e8
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-12-19 18:52:22 +0600

    Add files via upload

---
+++ C Programming/C practice from book/Math/m2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    double a1,a2,b1,b2,c1,c2,x,y;
+
+    
+     printf("a1 = ");
+     scanf("%lf",&a1);
+
+     printf("a2 = ");
+     scanf("%lf",&a2);
+
+
+     scanf("b1 = ");
+     scanf("%lf",&b1);
+    
+
+     printf("b2 = ");
+     scanf("%lf",&b2);
+
+     printf("c1 = ");
+     scanf("%lf",&c1);
+
+     printf("c2 = ");
+     scanf("%lf",&c2);
+
+     
+    return 0;
+
+}
