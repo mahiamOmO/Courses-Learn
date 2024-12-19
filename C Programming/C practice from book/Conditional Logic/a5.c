b9be72c15c4c346d39878ba966620df6fd16e00f
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-12-19 18:48:03 +0600

    Add files via upload

---
+++ C Programming/C practice from book/Conditional Logic/a5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int number = 12;
+    if(number > 10){
+        printf("The number is greater than ten\n");
+    }
+}
---
+++ C Programming/C practice from book/Conditional Logic/a8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num = 9;
+    if(num % 2 == 0){
+        printf("The number ias even\n");
+    }
+    else{
+        printf("The number is odd\n");
+    }
+    return 0;
+}
