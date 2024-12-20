7cfd91556eb6a112ad34eca465f989eb2a9103b6
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-12-20 17:56:01 +0600

    Delete C++/Bro code directory

--- C++/Bro code/dec.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-int main(){
-    int x = 5;
-    std:: cout<<x;
-
-    return 0;
-}
--- C++/Bro code/declaration.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-int main(){
-    int x;   //declaration
-
-    x = 5;  //assignment
-    std:: cout<<x;
-
-    return 0;
-}
